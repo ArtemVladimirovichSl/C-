8793fab928462fe7ab7c669f0b710888576ec386
Артём <slivnitsin.art@mail.ru>
2022-08-21 02:17:26 +0800

    Examples006_Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 1; // = - оператор присваивания
+int b = 2;
+int c = 6; // вводим данные
+int d = 8;
+int e = 4;
+
+int max = a; // за максимум берем значение первой гири
+
+if (a > max) max = a; // если значение а больше максимума, то а - максимум
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max); // вывод значения max на экран
