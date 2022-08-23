8b98a8203e4fbba45ad03cee5490d123ea53613d
Artem <slivnitsin.art@mail.ru>
2022-08-24 02:34:24 +0800

    ArrayLibrary

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -1,7 +1,7 @@
 ﻿void FillArray(int[] collection)                       //заполнение массива числами
 {
     int length = collection.Length;                    //определение длинны массива
-    int index =0;                                      //позиция индекса массива по умолчанию
+    int index = -1;                                    //позиция индекса массива по умолчанию (на консоли если элемент не найден)
     while(index < length)                              //пока индекс меньше длинны массива
     {
         collection[index] = new Random().Next(1, 10);  //заполнение массива случайными целыми числами от 1 до 10
@@ -18,7 +18,28 @@ void PrintArray(int[] col)                             //метод печати
         position++;                                    //увеличение позиции индекса элемента на 1
     }
 }
+int IndexOf(int[] collection, int find)                //метод возврата индекса
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = 0;                                  //переменная для сохранения позиции элемента
+    while(index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;                                     //остановка
+        }  
+        index++;
+    }
+    return position;                                   //возврат позиции элемента
+}
+
 int[] array = new int[10];                             //определение массива из десяти элементов
 
 FillArray(array);                                      //заполнение массива
 PrintArray(array);                                     //печать массива
+Console.WriteLine();                                   //вывод пустой строки
+
+int pos = IndexOf(array, 9);                           //результат работы IndexOf
+Console.WriteLine(pos);
